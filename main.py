7e6350c17c5ee109850e3109baf90e6001009121
williamgrimes <williamgrimes@gmx.com>
2023-03-27 12:15:04 +0200

    Log print statements as info

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """Zindi Africa palm tree counting competition."""
 from core import argparser, efficientnet
+import sys
+
 from core.logs import ProjectLogger
 
 logger = ProjectLogger(__name__)
 
 if __name__ == '__main__':
+    sys.stdout.write = logger.i  # Redirect stdout to logger to include print statements in log file
     parsed_args = argparser.parse_project_args()
 
     kwargs = vars(parsed_args)
@@ -15,6 +18,6 @@ if __name__ == '__main__':
 
     logger.setup_info(kwargs, exclude_list=["auth_token"])
 
-    command_map.get(parsed_args.command)(kwargs)
+    command_map.get(kwargs.get("command"))(kwargs)
 
 
